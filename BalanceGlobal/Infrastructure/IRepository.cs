5a2c34355d16bf015b15d77207ca2b8b24e815e0
Gerardo Rodríguez <gerardo.rodriguez05@gmail.com>
2020-10-01 12:42:03 -0300

    Cambios en repositorio

--- BalanceGlobal/Infrastructure/IRepository.cs
+++ BalanceGlobal/Infrastructure/IRepository.cs
@@ -8,13 +8,13 @@ namespace BalanceGlobal.Infrastructure
 {
     public interface IRepository<T> where T : class
     {
-        Task<T> GetById(string id);
+        Task<T> GetById(object id);
         //T Get(Expression<Func<T, bool>> where);
         Task<IEnumerable<T>> GetAllAsync();
         Task<IEnumerable<T>> GetManyAsync(Expression<Func<T, bool>> where);
         Task<IEnumerable<T>> GetWithStoredProcedureAsync(string spName, params object[] prms);
-        Task AddAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task RemoveAsync(object id);
+        Task AddAsync(T entity, string userName);
+        Task UpdateAsync(T entity, string userName);
+        Task RemoveAsync(object id, string userName);
     }
 }
