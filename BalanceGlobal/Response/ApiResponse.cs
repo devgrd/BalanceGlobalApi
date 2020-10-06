417de0b155359a066ad75635a647fe6f295c4555
Gerardo Rodríguez <gerardo.rodriguez05@gmail.com>
2020-10-05 21:26:48 -0300

    ApiResponse

---
+++ BalanceGlobal/Response/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BalanceGlobal.Response
+{
+    public class ApiResponse<T>
+    {
+        public int Code { get; set; }
+        public T Data { get; set; }
+    }
+}
